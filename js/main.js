6d3eb05ccea814b6b499a1b99d0742118dc6bb46
GianbaBotassi <gianba89@gmail.com>
2023-05-11 20:25:43 +0200

    Exercise done with prompt

--- js/main.js
+++ js/main.js
@@ -3,26 +3,80 @@
 // Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// Dichiaro un array vuoto per i numeri generati random
 const arrayFiveRandomNum = [];
 
-const play = document.getElementById('play');
+// Dichiaro un array vuoto per i numeri scelti dall'user
+const arrayFiveUserNum = [];
 
+// Associo bottone per cominciare partita
+const elGame = document.getElementById('game');
+
+// Associo div container per innerHTML
 const container = document.getElementById('container');
 
-play.addEventListener('click',
+
+let score = 0;
+
+let numIndovinati = [];
+
+
+
+
+
+elGame.addEventListener('click',
     () =>{
 
         innerRandNum();
 
-        setInterval(ciao,30000)
+        let clock = setInterval(ciao,10000)
 
 
         function ciao(){
+
+            clearInterval(clock);
+
             container.innerHTML = "";
+
+
+            // const ciao = createInputElement(5, container);
+
+            for(let i = 0; i < arrayFiveRandomNum.length; i++){
+
+                const numUser = parseInt(prompt("inserisci 5 numeri diversi tra loro"));
+                arrayFiveUserNum.push(numUser);
+
+            }
+
+            console.log(arrayFiveUserNum);
+            console.log(arrayFiveRandomNum);
+
+            let i = 0; 
+
+            while( i < arrayFiveUserNum.length){
+                const numUser = arrayFiveUserNum[i];
+
+                console.log(numUser);
+                if (arrayFiveRandomNum.includes(numUser)) {
+
+                    numIndovinati.push(numUser);
+
+                    score++;
+                    
+                }
+                i++
+            }
+            
+            const stringNumbers = numIndovinati.toString();
+
+            alert("hai totalizzato un punteggio di " + score + ". I numeri indovinati sono " + stringNumbers)
         }
+
     }
-    
     )
+
+
+
 
 
 
@@ -42,7 +96,7 @@ function mathRandomMinMax(min, max){
 function randomNumberArray(totNum, min, max, array){
     while(array.length < totNum){
         const newNumber = mathRandomMinMax(min, max);
-        if (!array.includes[newNumber]) {
+        if (!array.includes(newNumber)) {
             array.push(newNumber);
         }
     }
@@ -55,4 +109,14 @@ function innerRandNum(){
     for(let i = 0; i < arrayFiveRandomNum.length; i++){
         container.innerHTML += `<div>${arrayFiveRandomNum[i]}</div>`
     }
+}
+
+
+// Function to create new tag element
+function createInputElement(numElements, elemento){
+    for(let i = 0; i < numElements; i++){
+        const newElement = `<input type="text">`;
+        elemento.innerHTML = newElement;
+    }
+ 
 }
